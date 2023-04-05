2368ca940366fbd53cab40514e9d46831bef4e63
halilozler <ozler166@hotmail.com>
2023-04-05 10:41:51 +0300

    docker update

---
+++ src/main/java/com/example/javamilsoftbootcampfinalcase/test/Test.java
@@ -0,0 +1,18 @@
+package com.example.javamilsoftbootcampfinalcase.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class Test {
+
+    @GetMapping("/message")
+    @ResponseBody
+    public String getMessage(){
+        return "hello world";
+    }
+}
